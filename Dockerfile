4aaca8279e7e80f8cd53fe3f4bf99ee696f783b4
sharofiddin <pardayev.sharofiddin@gmail.com>
2025-04-24 13:03:24 +0500

    slots configured

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM postgres:16
 
 RUN apt-get update
 RUN apt-get install iputils-ping -y
-
+COPY ./slave-init.sh .
---
+++ slave-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+    echo "First time running. Init configuration."
+    until ping -c 1 -W 1 primary
+    do
+      echo "Waiting for leader to ping..."
+      sleep 1s
+    done
+    cd $PGDATA
+    su postgres
+    until pg_basebackup  -h primary -p 5432 -D . -U replicator -P -v -R -X stream -S ${SLOT_NAME}
+    do
+      echo "Waiting for pg_basebackup..."
+      sleep 1s
+    done
+    chmod -R 0700 $PGDATA
+    chown -R postgres:postgres $PGDATA
+fi
+su postgres -c "postgres -c config_file=/etc/postgresql/postgresql.conf"
