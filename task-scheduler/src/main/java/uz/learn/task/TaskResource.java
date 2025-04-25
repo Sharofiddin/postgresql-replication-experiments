989c0ad97094d1814c713c57c0dc07839db4af12
sharofiddin <pardayev.sharofiddin@gmail.com>
2025-04-25 19:27:23 +0500

    vertx pool

--- task-scheduler/src/main/java/uz/learn/task/TaskResource.java
+++ task-scheduler/src/main/java/uz/learn/task/TaskResource.java
@@ -1,30 +1,87 @@
 package uz.learn.task;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
-import io.quarkus.panache.common.Sort;
+import io.quarkus.reactive.datasource.ReactiveDataSource;
 import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+import io.vertx.mutiny.sqlclient.Pool;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowIterator;
+import io.vertx.mutiny.sqlclient.Tuple;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("tasks")
 @Produces("application/json")
 @Consumes("application/json")
 public class TaskResource {
-  
+
+ @Inject
+ private Pool primaryClient; 
+
+ @Inject
+ @ReactiveDataSource("replica-sync")
+ private Pool syncReplicaClient; 
+ 
+ @Inject
+ @ReactiveDataSource("replica-async")
+ private  Pool asyncReplicaClient; 
+
  @GET
  public Uni<List<Task>> tasks(){
-    return Task.listAll(Sort.by("scheduledAt"));
+    return getTasks(syncReplicaClient);
+	}
+
+
+private Uni<List<Task>> getTasks(Pool replicaClient) {
+	return replicaClient.query("""
+    		SELECT id, name, scheduledat, status FROM task
+    		""").execute().map(rows->{
+     List<Task> tasks = new ArrayList<>();
+      RowIterator<Row> iterator = rows.iterator();
+      while(iterator.hasNext()) {
+    	  Row row = iterator.next();
+    	  Task task = new Task();
+    	  task.id = row.getLong("id");
+    	  task.name = row.getString("name");
+    	  task.scheduledAt = row.getLocalDateTime("scheduledat");
+    	  task.status = row.getString("status");
+    	  tasks.add(task);
+      }
+      return tasks;
+    });
+}
+
+
+ @GET
+ @Path("/async")
+ public Uni<List<Task>> tasksAsync(){
+	 return getTasks(asyncReplicaClient);
 	}
 
   @POST
   @WithTransaction
-  public Uni<Response> create(Task task) {
-    task.status = "PENDING";
-    return task.persist().map(item->Response.ok(item).build());
+    public Uni<Response> create(Task task) {
+	return primaryClient.preparedQuery("""
+			INSERT INTO public.task(
+			   id,
+			   name, 
+			   scheduledat, 
+			   status) 
+			VALUES (
+			   (select nextval('task_seq')),
+			    $1, 
+			   $2, 
+			   $3)
+					""")
+	.execute(Tuple.of(task.name, task.scheduledAt, "PENDING"))
+//	.execute(Tuple.of(task.name, task.scheduledAt, "PENDING"))
+	.map(item->Response.ok().build());
  }
 }
